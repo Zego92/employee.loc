ce6ae9d214a80fd698de4bfa7de9f868a372760f
Zego <Timchenco92@gmail.com>
2020-10-12 17:28:14 +0300

    Finish tasks

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -2,19 +2,27 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Department;
 use App\Employee;
+use App\EmployeePaymentType;
 use Faker\Generator as Faker;
 use Illuminate\Support\Carbon;
 
 $factory->define(Employee::class, function (Faker $faker) {
     return [
+//        'department_id' => function() {
+//            return factory(Department::class)->create()->id;
+//        },
+//        'type_id' => function() {
+//            return factory(EmployeePaymentType::class)->create()->id;
+//        },
         'department_id' => rand(1,20),
         'type_id' => rand(1,2),
         'first_name' => $faker->name,
         'middle_name' => $faker->name,
         'last_name' => $faker->name,
         'b_day' => $faker->date(),
-        'start_work' => Carbon::now(),
+        'start_work' => Carbon::now()->subDays(20),
         'rank' => $faker->word,
     ];
 });
